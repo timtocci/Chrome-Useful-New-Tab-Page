1ca1339f11a15ffa42ab274db8f67ab38f654640
timtocci <timtocci@ymail.com>
2017-05-13 19:22:00 -0400

    remove whitespace

--- background.js
+++ background.js
@@ -22,8 +22,6 @@ function setDefaultOptionsData(){
     }]
     }, ()=>{});
 }
-
-
 //setDefaultOptionsData()
 chrome.storage.sync.get('optionsmenudata',(entries)=>{
     function isActive(menu) {
@@ -36,49 +34,37 @@ chrome.storage.sync.get('optionsmenudata',(entries)=>{
     }
     if (isActive("OpenMenu")) {
         const hp = chrome.contextMenus.create({"title": "Open..."});
-
         chrome.contextMenus.create({"title": "Bookmarks", "contexts": ["page"], "parentId": hp, "onclick": openBookmarks});
-
         function openBookmarks(i, t) {
             const createProperties = {url: "chrome://bookmarks"};
             chrome.tabs.create(createProperties);
         }
-
         chrome.contextMenus.create({"title": "History", "contexts": ["page"], "parentId": hp, "onclick": openHistory});
-
         function openHistory(i, t) {
             const createProperties = {url: "chrome://history"};
             chrome.tabs.create(createProperties);
         }
-
         chrome.contextMenus.create({"title": "Downloads", "contexts": ["page"], "parentId": hp, "onclick": openDownloads});
-
         function openDownloads(i, t) {
             const createProperties = {url: "chrome://downloads"};
             chrome.tabs.create(createProperties);
         }
-
         chrome.contextMenus.create({"title": "Settings", "contexts": ["page"], "parentId": hp, "onclick": openSettings});
-
         function openSettings(i, t) {
             const createProperties = {url: "chrome://settings"};
             chrome.tabs.create(createProperties);
         }
-
         chrome.contextMenus.create({
             "title": "Extensions",
             "contexts": ["page"],
             "parentId": hp,
             "onclick": openExtensions
         });
-
         function openExtensions(i, t) {
             const createProperties = {url: "chrome://extensions"};
             chrome.tabs.create(createProperties);
         }
-
         chrome.contextMenus.create({"title": "Apps", "contexts": ["page"], "parentId": hp, "onclick": openApps});
-
         function openApps(i, t) {
             const createProperties = {url: "chrome://apps"};
             chrome.tabs.create(createProperties);
@@ -86,7 +72,6 @@ chrome.storage.sync.get('optionsmenudata',(entries)=>{
         // any other menu init here
     }
 });
-
 function setActive(menu){
     chrome.storage.sync.get('optionsmenudata',(entries)=>{
         const elen = entries.optionsmenudata.length;
